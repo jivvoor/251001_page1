fae82a2863aabac37f38c543534ee8e55ee60ec9
jivvoor <wldn040839@gmail.com>
2025-10-01 12:33:13 +0900

    step

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { createClient } = require("@supabase/supabase-js"); // 구조분해 할당
+
+dotenv.config(); // .env -> KEY => SUPABASE_KEY
+// NODE -> process.env (환경변수) // cf. env file
+
+// const supabaseKey = process.env.SUPABASE_KEY;
+// const supabaseUrl = process.env.SUPABASE_URL;
+const { SUPABASE_KEY: supabaseKey, SUPABASE_URL: supabaseUrl } = process.env;
+console.log("supabaseKey", supabaseKey); // 확인 (npm run dev)
+console.log("supabaseUrl", supabaseUrl);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json()); // req.body -> json.
+
+app.get("/", (req, res) => {
+  res.send("bye");
+});
+
+app.get("/plans", async (req, res) => {
+  const { data, error } = await supabase.from("tour_plan").select("*");
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+  res.json(data);
+});
+
+app.post("/plans", async (req, res) => {
+  const plan = req.body;
+  const { error } = await supabase.from("tour_plan").insert(plan);
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+  res.status(201).json();
+});
+
+app.listen(port, () => {
+  console.log(`서버가 ${port}번 포트로 실행 중입니다.`);
+});
